eabef81ad4c3c3137357a155342a990673d75258
ferinmtk <ferinmtk@gmail.ocm>
2024-06-13 21:40:33 +0300

    Signed-off-by: ferinmtk <ferinmtk@gmail.ocm>

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("whats your grade Percentage");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = " ";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        
+        }
+        else if (percent >=60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Cheers!! You Passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("There is always a next time to do better!");
+
+        }
+
+        //adding a + or -
+
+        if (letter != "F" && letter != "A")
+        {
+            int lastDigit = percent % 10;
+            if (lastDigit >= 7)
+            {
+                letter += "+";
+            }
+            else if (lastDigit <3)
+            {
+                letter += "-";
+
+            }
+
+
+
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
     }
+
 }
