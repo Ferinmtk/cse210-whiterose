0b05f1070597db5d435f0d910485742b7755a09b
ferinmtk <ferinmtk@gmail.ocm>
2024-06-13 19:43:46 +0300

    Finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your Last Name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
