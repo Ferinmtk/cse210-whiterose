8c42a0f75d6a70121fae4f30af7b4dcd1aa29c53
ferinmtk <ferinmtk@gmail.ocm>
2024-06-19 00:44:03 +0300

    Done!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 10);
+
+        int guess = -1;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
